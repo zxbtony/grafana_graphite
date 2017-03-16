c854874a3d648c71a30e7343fbca34c007ecfa67
Tony Zhang <tony.zxb@outlook.com>
2017-03-16 14:07:14 +0800

    Update run.sh
    add graphite datasource

--- run.sh
+++ run.sh
@@ -83,4 +83,8 @@ if [ "$1" = 'start_server' ] || [ "$#" == 0 ]; then
     cfg:default.paths.plugins="$GF_PATHS_PLUGINS"
 fi
 
+if [[ -z `curl -s 'http://admin:admin@localhost:3000/api/datasources' | grep graphite` ]];then
+        curl 'http://admin:admin@localhost:3000/api/datasources' -X POST -H 'Content-Type:application/json;charset=UTF-8' --data-binary '{"name":"Graphite","type":"graphite","url":"http://localhost","access":"proxy","isDefault":true,"jsonData":{}}'
+fi
+
 exec "$@"
