7f13a9219c2ea5571973301d9fb1b4640a47d9fb
Tony Zhang <tony.zxb@outlook.com>
2017-03-16 15:21:00 +0800

    Update add_datasource.sh

--- add_datasource.sh
+++ add_datasource.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sleep 10000
+sleep 5
 
-if [[ -z `curl -s 'http://admin:admin@localhost:3000/api/datasources' | grep graphite` ]];then
+if [ -z `curl -s 'http://admin:admin@localhost:3000/api/datasources' | grep graphite` ]; then
       curl 'http://admin:admin@localhost:3000/api/datasources' -X POST -H 'Content-Type:application/json;charset=UTF-8' --data-binary '{"name":"Graphite","type":"graphite","url":"http://localhost","access":"proxy","isDefault":true,"jsonData":{}}'
 fi
 
